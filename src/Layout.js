12350aca143c7393605177d2c7861b89a955a196
nam nguyen <nguyenphuongnam284@gmail.com>
2024-10-04 10:47:56 +0700

    #70

---
+++ src/Layout.js
@@ -0,0 +1,49 @@
+import React from "react";
+import App from "./App";
+import User from "./components/User/User";
+import Admin from "./components/Admin/Admin";
+import HomePage from "./components/Home/HomePage";
+import ManageUser from "./components/Admin/Content/ManageUser";
+import DashBoard from "./components/Admin/Content/DashBoard";
+import ManageQuiz from "./components/Admin/Content/ManageQuiz";
+import ManageQuestions from "./components/Admin/Content/ManageQuestions";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Layout = (props) => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="users" element={<User />} />
+        </Route>
+        <Route path="/admin" element={<Admin />}>
+          <Route index element={<DashBoard />} />
+          <Route path="manage-users" element={<ManageUser />} />
+          <Route path="manage-questions" element={<ManageQuestions />} />
+          <Route path="manage-quizs" element={<ManageQuiz />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+};
+
+export default Layout;
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,14 +1,35 @@
 import React, { useState } from "react";
 import "./login.scss";
+import { useNavigate } from "react-router-dom";
+import { postLogin } from "../../services/apiServices";
+import { toast } from "react-toastify";
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleLogin = () => {
-    console.log(email + " " + password);
+
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    // validate
+
+    //submit
+    let res = await postLogin(email, password);
+    if (res && +res.EC === 0) {
+      toast.success(res.EM);
+      navigate("/");
+    }
+    if (res && +res.EC !== 0) {
+      toast.error(res.EM);
+    }
   };
+  const handleClickBtnRegister = () => {};
   return (
     <div className="login-container">
-      <div className="header">Don't have an account yet?</div>
+      <div className="header">
+        <span>Don't have an account yet?</span>
+        <button className="regis-btn" onClick={() => handleClickBtnRegister()}>
+          Sign up
+        </button>
+      </div>
       <div className="title col-4 my-2 mx-auto text-center">Login</div>
       <div className="welcome col-4 mx-auto text-center">
         Hello, who's this?
@@ -38,6 +59,9 @@ const Login = (props) => {
           <button className="login-btn" onClick={() => handleLogin()}>
             Login to website
           </button>
+        </div>
+        <div className="back">
+          <span onClick={() => navigate("/")}>&#60;&#60; Go to Homepage</span>
         </div>
       </div>
     </div>
